fcad8410df1a1b6688bbc7fa36d09889bedb5569
BoyPika <boypika85@gmail.com>
2023-06-23 13:54:43 -0400

    c

--- src/main/java/net/boypika/BossWhackers.java
+++ src/main/java/net/boypika/BossWhackers.java
@@ -13,19 +13,15 @@ import net.minecraft.util.Rarity;
 public class BossWhackers implements ModInitializer {
 	public static final ToolItem WITHER_WHACKER = new SwordItem(ToolMaterials.DIAMOND, 396, -3.5f, new FabricItemSettings().rarity(Rarity.UNCOMMON));
 	public static final ToolItem DRAGON_WHACKER = new SwordItem(ToolMaterials.NETHERITE, 795,-3.9f, new FabricItemSettings().fireproof().rarity(Rarity.EPIC));
-	public static final ToolItem WARDEN_WHACKER = new SwordItem(ToolMaterials.NETHERITE, 495, -3.6f, new FabricItemSettings().fireproof().rarity(Rarity.EPIC));
-	public static void registerItemGroups() {
+	public static final ToolItem WARDEN_WHACKER = new SwordItem(ToolMaterials.NETHERITE, 495, -3.6f, new FabricItemSettings().fireproof().rarity(Rarity.RARE));
 
+	@Override
+	public void onInitialize() {
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
 			entries.add(WITHER_WHACKER);
 			entries.add(DRAGON_WHACKER);
 			entries.add(WARDEN_WHACKER);
 		});
-	}
-
-	@Override
-	public void onInitialize() {
-		registerItemGroups();
 		Registry.register(Registries.ITEM, new Identifier("bosswhackers", "wither_whacker"), WITHER_WHACKER);
 		Registry.register(Registries.ITEM, new Identifier("bosswhackers","dragon_whacker"), DRAGON_WHACKER);
 		Registry.register(Registries.ITEM, new Identifier("bosswhackers", "warden_whacker"), WARDEN_WHACKER);
